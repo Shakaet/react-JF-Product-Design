4e012a576c5b4c2b1d43539eaf4e68e46877f766
userName <userEmail>
2025-07-29 17:46:25 +0600

    Customers section added

--- src/component/Customer.jsx
+++ src/component/Customer.jsx
@@ -1,8 +1,112 @@
 import React from 'react'
+import banner from "../assets/banner2.png"
+import human from "../assets/human.png"
 
 const Customer = () => {
   return (
-    <div>Customer</div>
+    <div className='mt-16'>
+
+
+        <div>
+            <h2 style={{fontSize:"42px"}} className='text-center font-bold mb-10'>Why Customers Love ❤️ Us?</h2>
+        </div>
+
+
+
+        <div className='flex gap-10 rounded-2xl ps-20 pr-20'>
+
+
+            <div style={{width:"881px",height:"608px"}} className='bg-[#F2F4F6] rounded-2xl '>
+
+               <div className=' mt-17 ms-14 '>
+                 <div className='flex gap-1 mb-10'>
+                    <svg width="32.458496" height="59.791748" viewBox="0 0 32.4585 59.7917" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<desc>
+			Created with Pixso.
+	</desc>
+	<defs/>
+	<path id="Vector" d="M6.83 23.91C6.83 14.48 14.48 6.83 23.91 6.83C25.8 6.83 27.33 5.3 27.33 3.41C27.33 1.52 25.8 0 23.91 0C10.7 0 0 10.7 0 23.91L0 50.39C0 55.58 4.2 59.79 9.39 59.79L23.06 59.79C28.25 59.79 32.45 55.58 32.45 50.39L32.45 36.72C32.45 31.54 28.25 27.33 23.06 27.33L9.39 27.33C8.5 27.33 7.64 27.45 6.83 27.68L6.83 23.91Z" fill="#B0DD1D" fill-opacity="1.000000" fill-rule="nonzero"/>
+</svg>
+<svg width="32.458496" height="59.791748" viewBox="0 0 32.4585 59.7917" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<desc>
+			Created with Pixso.
+	</desc>
+	<defs/>
+	<path id="Vector" d="M6.83 23.91C6.83 14.48 14.48 6.83 23.91 6.83C25.8 6.83 27.33 5.3 27.33 3.41C27.33 1.52 25.8 0 23.91 0C10.7 0 0 10.7 0 23.91L0 50.39C0 55.58 4.2 59.79 9.39 59.79L23.06 59.79C28.25 59.79 32.45 55.58 32.45 50.39L32.45 36.72C32.45 31.54 28.25 27.33 23.06 27.33L9.39 27.33C8.5 27.33 7.64 27.45 6.83 27.68L6.83 23.91Z" fill="#B0DD1D" fill-opacity="1.000000" fill-rule="nonzero"/>
+</svg>
+
+                </div>
+
+                <p className='mt-10' style={{fontSize:"28px"}}>I had initial issue with stock availability and delivery. <br /> Once the product arrived on site the work has <br /> completed very fast(400ft.) Value for money and easy <br /> to install. Happy with the product</p>
+                  
+
+                  <div className='flex justify-between mt-34 pt-10 pb-10 ps-5 pr-10 mr-10 rounded-3xl' style={{ backgroundImage: `url(${banner})`}}>
+
+                    <div>
+
+                        <h2 style={{fontSize:"32px"}}  className='font-bold'>- Samuel Varughese</h2>
+
+                    </div>
+
+                    <div className='flex gap-5'>
+
+                        <svg width="51.000000" height="51.000000" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<desc>
+			Created with Pixso.
+	</desc>
+	<defs>
+		<clipPath id="clip243_243">
+			<rect id="Frame" width="24.000000" height="24.000000" transform="matrix(-1 0 0 1 37.5 13.5)" fill="white" fill-opacity="0"/>
+		</clipPath>
+	</defs>
+	<rect id="Frame" width="24.000000" height="24.000000" transform="matrix(-1 0 0 1 37.5 13.5)" fill="#FFFFFF" fill-opacity="0"/>
+	<g clip-path="url(#clip243_243)">
+		<path id="Vector" d="M17.5 25.5L33.5 25.5" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M17.5 25.5L33.5 25.5" stroke="#000000" stroke-opacity="0.200000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M22.5 30.5C22.5 30.5 17.5 26.81 17.5 25.5C17.5 24.18 22.5 20.5 22.5 20.5" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M22.5 30.5C22.5 30.5 17.5 26.81 17.5 25.5C17.5 24.18 22.5 20.5 22.5 20.5" stroke="#000000" stroke-opacity="0.200000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+	</g>
+	<rect id="Frame 432" rx="25.078125" width="50.156250" height="50.156250" transform="matrix(-1 0 0 1 51.4219 0.421875)" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="0.843750"/>
+	<rect id="Frame 432" rx="25.078125" width="50.156250" height="50.156250" transform="matrix(-1 0 0 1 51.4219 0.421875)" stroke="#000000" stroke-opacity="0.200000" stroke-width="0.843750"/>
+</svg>
+<svg width="51.000000" height="51.000000" viewBox="0 0 51 51" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<desc>
+			Created with Pixso.
+	</desc>
+	<defs>
+		<clipPath id="clip243_239">
+			<rect id="Frame" width="24.000000" height="24.000000" transform="translate(13.500000 13.500000)" fill="white" fill-opacity="0"/>
+		</clipPath>
+	</defs>
+	<rect id="Frame" width="24.000000" height="24.000000" transform="translate(13.500000 13.500000)" fill="#FFFFFF" fill-opacity="0"/>
+	<g clip-path="url(#clip243_239)">
+		<path id="Vector" d="M33.5 25.5L17.5 25.5" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M33.5 25.5L17.5 25.5" stroke="#000000" stroke-opacity="0.200000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M28.5 30.5C28.5 30.5 33.5 26.81 33.5 25.5C33.5 24.18 28.5 20.5 28.5 20.5" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+		<path id="Vector" d="M28.5 30.5C28.5 30.5 33.5 26.81 33.5 25.5C33.5 24.18 28.5 20.5 28.5 20.5" stroke="#000000" stroke-opacity="0.200000" stroke-width="1.265625" stroke-linejoin="round" stroke-linecap="round"/>
+	</g>
+	<rect id="Frame 394" rx="25.078125" width="50.156250" height="50.156250" transform="translate(0.421875 0.421875)" stroke="#1A1A1A" stroke-opacity="1.000000" stroke-width="0.843750"/>
+	<rect id="Frame 394" rx="25.078125" width="50.156250" height="50.156250" transform="translate(0.421875 0.421875)" stroke="#000000" stroke-opacity="0.200000" stroke-width="0.843750"/>
+</svg>
+
+
+                    </div>
+
+
+                  </div>
+               </div>
+            </div>
+
+            <div className='bg-[#F2F4F6] flex justify-center items-center rounded-2xl pr-5 ps-5' style={{width:"359px",height:"608px"}}>
+
+
+                <img style={{width:"309px",height:"568px"}} src={human} alt="" />
+
+
+
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/component/Trusted.jsx
+++ src/component/Trusted.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import img from "../assets/image.png"
+
 
 const Trusted = () => {
   return (
